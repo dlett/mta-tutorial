2f74bf25631ba81b52bec3068a2ac08e003e9899
Daniel <daniel@lb-dev.org>
2018-02-19 15:20:57 -0500

    Added initial database schema.

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `vehicles` (
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `model` INTEGER NOT NULL,
+    `x` REAL NOT NULL,
+    `y` REAL NOT NULL,
+    `z` REAL NOT NULL
+)
